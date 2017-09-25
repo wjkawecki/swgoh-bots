052d8edaca119ecd74495ab20b5736eca16cd011
Wojciech Kawecki <wojciech.kawecki@unic.com>
2017-09-25 11:56:26 +0200

    gfsd

--- src/Bot.js
+++ src/Bot.js
@@ -1,59 +1,59 @@
-import Discord from 'discord.js'
-import Nightmare from 'nightmare'
-import XLSX from 'xlsx'
+import Discord from 'discord.js';
+import Nightmare from 'nightmare';
+import XLSX from 'xlsx';
 
-import path from 'path'
+import path from 'path';
 
 const nightmare = Nightmare({
 	show: true,
 	openDevTools: {
 		mode: 'detach'
 	}
-})
+});
 
-const readChannelId = '360337936203382796'
-const writeChannelId = '360337936203382796'
-const xlsxUrl = 'https://onedriv	e.live.com/view.aspx?resid=B92275A9BF72C170!40986&ithint=file%2cxlsx&app=Excel&authkey=!AJDJRXahXJTfaUU'
+const readChannelId = '360337936203382796';
+const writeChannelId = '360337936203382796';
+const xlsxUrl = 'https://onedriv	e.live.com/view.aspx?resid=B92275A9BF72C170!40986&ithint=file%2cxlsx&app=Excel&authkey=!AJDJRXahXJTfaUU';
 
 export default class Bot {
 	constructor(botToken) {
-		this.main = this.main.bind(this)
+		this.main = this.main.bind(this);
 
-		this.botToken = botToken
+		this.botToken = botToken;
 
-		this.client = new Discord.Client()
+		this.client = new Discord.Client();
 		this.client.on("ready", async () => {
-			this.client.user.setGame('live countdowns until payout')
-			this.readChannel = this.client.channels.get(readChannelId)
-			this.writeChannel = this.client.channels.get(writeChannelId)
+			this.client.user.setGame('live countdowns until payout');
+			this.readChannel = this.client.channels.get(readChannelId);
+			this.writeChannel = this.client.channels.get(writeChannelId);
 
-			this.initializeBot()
-			console.log('Bot initialized')
-		})
+			this.initializeBot();
+			console.log('Bot initialized');
+		});
 
-		this.client.login(botToken)
+		this.client.login(botToken);
 
 		// scrape Excel online
 		// TODO not working right now, nightmare is unable to find elements by selectors
 		// await this.scrapeXlsx()
 		// TODO workaround: parse local xlsx file
-		this.sheet = XLSX.utils.sheet_to_json(XLSX.readFile(path.resolve(__dirname, '../SWGoH_Shard.xlsx')).Sheets.Sheet1)
+		this.sheet = XLSX.utils.sheet_to_json(XLSX.readFile(path.resolve(__dirname, '../SWGoH_Shard.xlsx')).Sheets.Sheet1);
 
-		this.parseXlsx()
+		this.parseXlsx();
 
-		this.main()
+		this.main();
 	}
 
 	async main() {
 		try {
 			if (this.message) {
-				this.calculateSecondsUntilPayout()
-				await this.sendMessage()
+				this.calculateSecondsUntilPayout();
+				await this.sendMessage();
 			}
 		} catch (err) {
-			console.log(err)
+			console.log(err);
 		} finally {
-			setTimeout(this.main, 60000 - Date.now() % 60000)
+			setTimeout(this.main, 60000 - Date.now() % 60000);
 		}
 	}
 
@@ -63,19 +63,19 @@ export default class Bot {
 		for (let i in a) {
 		  await a[i].delete()
 		} */
-		const messages = await this.writeChannel.fetchMessages()
+		const messages = await this.writeChannel.fetchMessages();
 		if (messages.array().length === 0) {
 			try {
-				this.message = await this.writeChannel.send({embed: new Discord.RichEmbed()})
+				this.message = await this.writeChannel.send({embed: new Discord.RichEmbed()});
 			} catch (err) {
-				console.log(err)
+				console.log(err);
 			}
 		} else {
 			if (messages.first().embeds.length === 0) {
-				await messages.first().delete()
-				this.message = await this.writeChannel.send({embed: new Discord.RichEmbed()})
+				await messages.first().delete();
+				this.message = await this.writeChannel.send({embed: new Discord.RichEmbed()});
 			} else {
-				this.message = messages.first()
+				this.message = messages.first();
 			}
 		}
 	}
@@ -87,81 +87,90 @@ export default class Bot {
 				.wait(5000)
 				.evaluate(() => {
 					// console.log(document.querySelector('#m_excelWebRenderer_ewaCtl_commandUIPlaceHolder'))
-					return document.querySelector('body')
+					return document.querySelector('body');
 				})
 				.end()
 				.then(res => {
-					console.log(res)
-				})
+					console.log(res);
+				});
 		} catch (err) {
-			console.error(err)
+			console.error(err);
 		}
 	}
 
 	parseXlsx() {
 		this.mates = []
 		for (let i in this.sheet) {
-			const user = this.sheet[i]
+			const user = this.sheet[i];
 			this.mates.push({
 				name: user.Name,
 				payout: parseInt(user.UTC.substr(0, 2)),
 				discordId: user.ID,
 				flag: user.Flag,
 				swgoh: user.SWGOH
-			})
+			});
 		}
-		const matesByTime = {}
+		const matesByTime = {};
 		for (let i in this.mates) {
-			const mate = this.mates[i]
+			const mate = this.mates[i];
 			if (!matesByTime[mate.payout]) {
 				matesByTime[mate.payout] = {
 					payout: mate.payout,
 					mates: []
-				}
+				};
 			}
-			matesByTime[mate.payout].mates.push(mate)
+			matesByTime[mate.payout].mates.push(mate);
 		}
-		this.mates = Object.values(matesByTime)
+		this.mates = Object.values(matesByTime);
 	}
 
 	calculateSecondsUntilPayout() {
-		const now = new Date()
+		const now = new Date();
 		for (let i in this.mates) {
-			const mate = this.mates[i]
-			const p = new Date()
-			p.setUTCHours(mate.payout, 0, 0, 0)
-			if (p < now) p.setDate(p.getDate() + 1)
-			mate.timeUntilPayout = p.getTime() - now.getTime()
-			let dif = new Date(mate.timeUntilPayout)
-			const round = dif.getTime() % 60000
+			const mate = this.mates[i];
+			const p = new Date();
+			p.setUTCHours(mate.payout, 0, 0, 0);
+			if (p < now) p.setDate(p.getDate() + 1);
+			mate.timeUntilPayout = p.getTime() - now.getTime();
+			let dif = new Date(mate.timeUntilPayout);
+			const round = dif.getTime() % 60000;
 			if (round < 30000) {
-				dif.setTime(dif.getTime() - round)
+				dif.setTime(dif.getTime() - round);
 			} else {
-				dif.setTime(dif.getTime() + 60000 - round)
+				dif.setTime(dif.getTime() + 60000 - round);
 			}
-			mate.time = `${String(dif.getUTCHours()).padStart(2, '00')}:${String(dif.getUTCMinutes()).padStart(2, '00')}`
+			mate.time = `${String(dif.getUTCHours()).padStart(2, '00')}:${String(dif.getUTCMinutes()).padStart(2, '00')}`;
 		}
 		this.mates.sort((a, b) => {
-			return a.timeUntilPayout - b.timeUntilPayout
+			return a.timeUntilPayout - b.timeUntilPayout;
 		})
 	}
 
 	async sendMessage() {
-		let embed = new Discord.RichEmbed().setColor(0x00AE86).setThumbnail('https://swgoh.gg/static/img/swgohgg-nav.png')
-		let desc = '**Next payout**:'
+		let embed = new Discord.RichEmbed(),
+			desc = '',
+			mate;
+
 		for (let i in this.mates) {
-			desc += `\n\`${this.mates[i].time}\`   `
+			desc += `\n\`${this.mates[i].time}\`   `;
 			for (let j in this.mates[i].mates) {
-				const mate = this.mates[i].mates[j]
-				desc += `${mate.flag} [${mate.name}](${mate.swgoh})   `
+				mate = this.mates[i].mates[j];
+				desc += `${mate.flag} [${mate.name}](${mate.swgoh})    `;
 			}
 
-			if (i === 0) {
-				desc += `\n\n\Following payouts`
+			if (i === '0') {
+				desc += '\n\n\Following payouts:';
 			}
 		}
-		embed.setDescription(desc)
-		await this.message.edit({embed})
+
+		embed
+			.setDescription(desc)
+			.setColor(0x00AE86)
+			.setThumbnail('https://swgoh.gg/static/img/swgohgg-nav.png')
+			.setAuthor('Next payout:')
+			.setTimestamp();
+
+		await this.message.edit({embed});
 
 		/*10:00 GMT
 		Pinnnkky
