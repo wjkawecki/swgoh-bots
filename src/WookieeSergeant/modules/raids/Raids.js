03cdb71c6e1b2d346aec1974e7dae6d95a9f1d7b
Wojciech Kawecki <wojciech.kawecki@unic.com>
2017-10-24 13:14:56 +0200

    update

--- src/WookieeSergeant/modules/raids/Raids.js
+++ src/WookieeSergeant/modules/raids/Raids.js
@@ -10,13 +10,13 @@ const jsonPath = '../../../../data/raids.json',
 	roles = {
 		officer: '<@&324139861709946901>',
 		shavedWookiee: '<@&324184776871510016>'
-	};
+	},
+	timeouts = [];
 
 export default class Raids {
 	constructor(Client) {
 		console.log('WookieeSergeant.Raids ready');
 		this.initChannels(Client, channels);
-		this.main = this.main.bind(this);
 		this.main();
 	}
 
@@ -30,6 +30,15 @@ export default class Raids {
 
 	async main() {
 		try {
+			console.log('WookieeSergeant.Raids.main()');
+
+			// const messages = await this.channels.bot_playground.fetchMessages();
+			// if (messages) {
+			// 	messages.forEach(async (message) => {
+			// 		await message.delete();
+			// 	});
+			// }
+
 			this.readJSON();
 			this.processRaids();
 		} catch (err) {
@@ -42,22 +51,25 @@ export default class Raids {
 	}
 
 	processRaids() {
-		this.findNextEvents();
-		this.setTimeouts();
+		this.findNextEvent();
+		this.setTimeout();
 	}
 
 	updateJSON() {
 		fs.writeFileSync(path.resolve(__dirname, jsonPath), JSON.stringify(this.json));
 	}
 
-	findNextEvents() {
+	findNextEvent() {
 		let now = new Date(),
-			nowHour = now.getUTCHours();
-
-		this.nextEvents = [];
+			nowHour = now.getUTCHours(),
+			nextEvents = [];
 
 		for (let raid in this.json) {
-			let nextEvent = {};
+			let nextEvent = {},
+				now = new Date(),
+				nextEventTime = new Date(),
+				diff;
+
 			nextEvent.type = raid;
 			raid = this.json[raid];
 
@@ -66,59 +78,64 @@ export default class Raids {
 				nextEvent.phase = raid.active.phase + 1;
 			} else if (raid.next) {
 				nextEvent.hour = raid.next.rotationTimeUTC;
+				nextEvent.reminderTriggered = raid.next.reminderTriggered;
 				nextEvent.phase = 0;
 			} else {
 				nextEvent.hour = raid.config.rotationTimesUTC.filter(this.findNextLaunchHour(nowHour))[0] || raid.config.rotationTimesUTC[0];
+				nextEvent.reminderTriggered = false;
 				nextEvent.phase = 0;
 			}
 
+			nextEventTime.setUTCHours(nextEvent.hour, 0, 0, 0);
+			if (nextEventTime < now) nextEventTime.setDate(nextEventTime.getDate() + 1);
+			diff = nextEventTime.getTime() - now.getTime();
+
+			nextEvent.diff = diff;
 			nextEvent.config = raid.config;
 
-			this.nextEvents.push(nextEvent);
+			nextEvents.push(nextEvent);
 		}
+
+		nextEvents.sort(function (a, b) {
+			return a.diff - b.diff;
+		});
+
+		this.nextEvent = nextEvents[0];
 	}
 
-	setTimeouts() {
-		let now = new Date();
+	setTimeout() {
+		let raid = this.nextEvent;
 
-		for (let raid of this.nextEvents) {
-			let raidTime = new Date(),
-				diff;
+		console.log('WookieeSergeant.Raids.setTimeout()');
+		console.log(raid);
 
-			console.log(raid);
+		if (raid.phase === 0 && !raid.reminderTriggered) { // remind @Officer to start raid
+			setTimeout(() => {
+				this.channels.bot_playground.send(`${roles.officer} start ${raid.type} NOW! After that type here "--start ${raid.type}"`);
 
-			raidTime.setUTCHours(raid.hour, 0, 0, 0);
-			if (raidTime < now) raidTime.setDate(raidTime.getDate() + 1);
-			diff = raidTime.getTime() - now.getTime();
+				this.json[raid.type].next.reminderTriggered = true;
 
-			console.log('diff: ' + diff / 60000 / 60);
+				this.updateJSON();
+				this.main();
+			}, raid.diff);
+		} else if (raid.phase > 0 && raid.phase <= raid.config.phases.count) { // remind @Shaved Wookiee about open phase
+			let nextPhase = (raid.config.phases.count > 1) ? `Phase ${raid.phase} ` : '';
 
-			if (raid.phase === 0) { // remind @Officer to start raid
-				setTimeout(() => {
-					this.channels.bot_playground.send(`${roles.officer} start ${raid.type} NOW! After that type here "--start ${raid.type}"`);
+			setTimeout((isLastPhase = (raid.phase === raid.config.phases.count)) => {
+				this.channels.bot_playground.send(`${roles.shavedWookiee} ${nextPhase}${raid.type} is now OPEN!`);
 
-					this.main;
-				}, diff);
-			} else if (raid.phase > 0 && raid.phase <= raid.config.phases.count) { // remind @Shaved Wookiee about open phase
-				let nextPhase = (raid.config.phases.count > 1) ? `Phase ${raid.phase} ` : '';
+				if (isLastPhase) { // this was the last phase - move raid to logs
+					this.json[raid.type].log.push(this.json[raid.type].active);
+					this.json[raid.type].active = null;
+				} else {
+					this.json[raid.type].active.phase++;
+				}
 
-				setTimeout((isLastPhase = (raid.phase === raid.config.phases.count), json = this.json) => {
-					this.channels.bot_playground.send(`${roles.shavedWookiee} ${nextPhase}${raid.type} is now OPEN!`);
-
-					if (isLastPhase) { // this was the last phase - move raid to logs
-						json[raid.type].log.push(json[raid.type].active);
-						json[raid.type].active = null;
-					} else {
-						json[raid.type].active.phase++;
-					}
-
-					this.updateJSON();
-					this.main;
-				}, diff);
-			}
+				this.updateJSON();
+				this.main();
+			}, raid.diff);
 		}
 	}
-
 
 	findNextLaunchHour(nowHour) {
 		return function (rotationTimesUTC) {
--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import { botToken } from './secret';
 
-import BB8 from './BB8/BB8';
-new BB8(botToken.BB8);
+// import BB8 from './BB8/BB8';
+// new BB8(botToken.BB8);
 
 import WookieeSergeant from './WookieeSergeant/WookieeSergeant';
 new WookieeSergeant(botToken.WookieeSergeant);
