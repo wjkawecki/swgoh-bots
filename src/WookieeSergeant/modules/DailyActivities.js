db2802e10c837e28b4d2013898a0f03302f701c9
Wojciech Kawecki <wojciech.kawecki@unic.com>
2017-11-02 13:13:02 +0100

    update

--- src/WookieeSergeant/modules/DailyActivities.js
+++ src/WookieeSergeant/modules/DailyActivities.js
@@ -1,4 +1,5 @@
 const DEV = true;
+import Discord from 'discord.js';
 
 const channels = {
 		the_guild_lounge: '324023862771712011',
@@ -18,7 +19,6 @@ export default class DailyActivities {
 		console.log(`WookieeSergeant.DailyActivities ready${DEV ? ' (DEV mode)' : ''}`);
 
 		this.Client = Client;
-		this.timeouts = [];
 
 		this.initChannels(channels);
 		this.listenToMessages();
@@ -29,9 +29,7 @@ export default class DailyActivities {
 		try {
 			console.log('WookieeSergeant.DailyActivities.main()');
 
-			// do something
-			// this.clearTimeout();
-			this.setTimeout();
+			this.scheduleReminder();
 		} catch (err) {
 			console.log(err);
 		}
@@ -63,8 +61,83 @@ export default class DailyActivities {
 		});
 	}
 
-	setTimeout() {
+	scheduleReminder() {
+		let now = new Date(),
+			reset = new Date(now),
+			resetDay,
+			diff;
 
+		reset.setUTCHours(resetTimeUTC.hour, resetTimeUTC.minute, 0, 0);
+		if (reset < now) reset.setDate(reset.getDate() + 1);
+		this.resetDay = reset.getUTCDay();
+		diff = reset.getTime() - now.getTime();
+
+		console.log(`WookieeSergeant.DailyActivities.scheduleReminder(): ${this.getReadableTime(diff)} to reset`);
+
+		// this.timeouts.push(setTimeout(() => {
+		// 	this.channels.the_guild_lounge.send(`<@&${roles.shavedWookiee}> 2 hours left to get your 600 daily tickets.`);
+		// }, diff - (2 * 60 * 60 * 1000)));
+
+		setTimeout((resetDay = this.resetDay) => {
+			let embed = new Discord.RichEmbed(),
+				activity = '',
+				desc = '';
+
+			console.log('resetDay ' + resetDay);
+
+			switch (resetDay) {
+
+				case 0: // Sunday
+					activity = 'Cantina Battles';
+					desc = `:zap: **Spend** Cantina Energy\n:x: **Save** Normal Energy\n:x: **Save** Galactic War (unless reset available)`;
+					console.log(reset.getUTCDay());
+					break;
+
+				case 1: // Monday
+					activity = 'Light Side Battles';
+					desc = `:zap: **Spend** Normal Energy on Light Side Battles\n:x: **Save** Galactic War Battles`;
+					console.log(reset.getUTCDay());
+					break;
+
+				case 2: // Tuesday
+					activity = 'Galactic War Battles';
+					desc = `:boom: **Complete** Galactic War Battles\n:x: **Save** Normal Energy`;
+					console.log(reset.getUTCDay());
+					break;
+
+				case 3: // Wednesday
+					activity = 'Hard Mode Battles';
+					desc = `:zap: **Spend** Normal Energy on Hard Mode Battles\n:x: **Save** Challenges`;
+					console.log(reset.getUTCDay());
+					break;
+
+				case 4: // Thursday
+					activity = 'Challenges';
+					desc = `:boom: **Complete** Challenges\n:x: **Save** Normal Energy`;
+					console.log(reset.getUTCDay());
+					break;
+
+				case 5: // Friday
+					activity = 'Dark Side Battles';
+					desc = `:zap: **Spend** Normal Energy on Dark Side Battles\n:x: **Save** Arena Battles\n:x: **Save** Cantina Energy`;
+					console.log(reset.getUTCDay());
+					break;
+
+				case 6: // Saturday
+					activity = 'Arena Battles';
+					desc = `:boom: **Complete** Arena Battles\n:x: **Save** Cantina Energy\n:x: **Save** Normal Energy`;
+					console.log(reset.getUTCDay());
+					break;
+			}
+
+			embed
+				.setDescription(desc)
+				.setColor(0x00bc9d);
+
+			this.channels.the_guild_lounge.send(embed);
+
+			this.main();
+		}, diff);
 	}
 
 	helpReply(msg) {
@@ -73,5 +146,17 @@ export default class DailyActivities {
 
 	isBotMentioned(msg) {
 		return msg.mentions.users.has(this.Client.user.id);
+	}
+
+	getReadableTime(time, showSeconds = false) {
+		time = new Date(time);
+
+		if (showSeconds) {
+			time = `${String(time.getUTCHours()).padStart(2, '00')}:${String(time.getUTCMinutes()).padStart(2, '00')}:${String(time.getUTCSeconds()).padStart(2, '00')}`;
+		} else {
+			time = `${String(time.getUTCHours()).padStart(2, '00')}:${String(time.getUTCMinutes()).padStart(2, '00')}`;
+		}
+
+		return time;
 	}
 }
