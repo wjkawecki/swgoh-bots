0d6f7ada2fe55dabb9cb5f309515809f725d2904
Wojciech Kawecki <wojciech.kawecki@unic.com>
2017-11-03 14:48:51 +0100

    update

--- src/WookieeSergeant/modules/DailyActivities.js
+++ src/WookieeSergeant/modules/DailyActivities.js
@@ -22,6 +22,11 @@ export default class DailyActivities {
 
 		this.initChannels(channels);
 		this.listenToMessages();
+
+		if (DEV) {
+			this.clearChannel(this.channels.bot_playground, true);
+		}
+
 		this.main();
 	}
 
@@ -74,9 +79,9 @@ export default class DailyActivities {
 
 		console.log(`WookieeSergeant.DailyActivities.scheduleReminder(): ${this.getReadableTime(diff)} to reset`);
 
-		// this.timeouts.push(setTimeout(() => {
-		// 	this.channels.the_guild_lounge.send(`<@&${roles.shavedWookiee}> 2 hours left to get your 600 daily tickets.`);
-		// }, diff - (2 * 60 * 60 * 1000)));
+		setTimeout(() => {
+			this.channels.the_guild_lounge.send(`<@&${roles.shavedWookiee}> 2 hours left to get your 600 daily tickets. Go grab them now!`);
+		}, diff - (2 * 60 * 60 * 1000));
 
 		setTimeout((resetDay = this.resetDay) => {
 			let embed = new Discord.RichEmbed(),
@@ -89,48 +94,64 @@ export default class DailyActivities {
 
 				case 0: // Sunday
 					activity = 'Cantina Battles';
-					desc = `:zap: **Spend** Cantina Energy\n:x: **Save** Normal Energy\n:x: **Save** Galactic War (unless reset available)`;
+					desc = `
+:zap:  **Spend** Cantina Energy
+:heavy_multiplication_x:  **Save** Normal Energy
+:heavy_multiplication_x:  **Save** Galactic War Battles (unless reset available)`;
 					console.log(reset.getUTCDay());
 					break;
 
 				case 1: // Monday
 					activity = 'Light Side Battles';
-					desc = `:zap: **Spend** Normal Energy on Light Side Battles\n:x: **Save** Galactic War Battles`;
+					desc = `
+:zap:  **Spend** Normal Energy on Light Side Battles
+:heavy_multiplication_x:  **Save** Galactic War Battles`;
 					console.log(reset.getUTCDay());
 					break;
 
 				case 2: // Tuesday
 					activity = 'Galactic War Battles';
-					desc = `:boom: **Complete** Galactic War Battles\n:x: **Save** Normal Energy`;
+					desc = `
+:boom:  **Complete** Galactic War Battles
+:heavy_multiplication_x:  **Save** Normal Energy`;
 					console.log(reset.getUTCDay());
 					break;
 
 				case 3: // Wednesday
 					activity = 'Hard Mode Battles';
-					desc = `:zap: **Spend** Normal Energy on Hard Mode Battles\n:x: **Save** Challenges`;
+					desc = `
+:zap:  **Spend** Normal Energy on Hard Mode Battles
+:heavy_multiplication_x:  **Save** Challenges`;
 					console.log(reset.getUTCDay());
 					break;
 
 				case 4: // Thursday
 					activity = 'Challenges';
-					desc = `:boom: **Complete** Challenges\n:x: **Save** Normal Energy`;
+					desc = `
+:boom:  **Complete** Challenges
+:heavy_multiplication_x:  **Save** Normal Energy`;
 					console.log(reset.getUTCDay());
 					break;
 
 				case 5: // Friday
 					activity = 'Dark Side Battles';
-					desc = `:zap: **Spend** Normal Energy on Dark Side Battles\n:x: **Save** Arena Battles\n:x: **Save** Cantina Energy`;
+					desc = `
+:zap:  **Spend** Normal Energy on Dark Side Battles
+:heavy_multiplication_x:  **Save** Arena Battles`;
 					console.log(reset.getUTCDay());
 					break;
 
 				case 6: // Saturday
 					activity = 'Arena Battles';
-					desc = `:boom: **Complete** Arena Battles\n:x: **Save** Cantina Energy\n:x: **Save** Normal Energy`;
+					desc = `
+:boom:  **Complete** Arena Battles
+:heavy_multiplication_x:  **Save** Cantina Energy`;
 					console.log(reset.getUTCDay());
 					break;
 			}
 
 			embed
+				.setAuthor(`New Guild Activity: ${activity}`)
 				.setDescription(desc)
 				.setColor(0x00bc9d);
 
@@ -158,5 +179,24 @@ export default class DailyActivities {
 		}
 
 		return time;
+	}
+
+	async clearChannel(channel, removeAll = false) {
+		console.log(`WookieeSergeant.DailyActivities.clearChannel()`);
+
+		if (removeAll) {
+			const messages = await channel.fetchMessages();
+
+			if (messages) {
+				messages.forEach(async (message) => {
+					await message.delete();
+				});
+			}
+		} else {
+			const message = await channel.fetchMessage(this.lastMessageId);
+
+			if (message)
+				await message.delete();
+		}
 	}
 }
