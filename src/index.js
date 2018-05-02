9ed3c21acb8805dcbeab6f5c70e7939a27f74894
wjkawecki <wjkawecki@proarray.com>
2018-05-03 00:05:40 +0200

    Update index.js

--- src/index.js
+++ src/index.js
@@ -4,10 +4,10 @@ import Heroku from './Heroku/Heroku';
 import BB8 from './BB8/BB8';
 import CloneSergeant from './CloneSergeant/CloneSergeant';
 import EwokSergeant from './EwokSergeant/EwokSergeant';
-import WookieeSergeant from './WookieeSergeant/WookieeSergeant';
+// import WookieeSergeant from './WookieeSergeant/WookieeSergeant';
 
 new Heroku();
 new BB8();
 new CloneSergeant();
 new EwokSergeant();
-new WookieeSergeant();
+// new WookieeSergeant();
